8e81a6f18ef9a8c365b37bd03fbf0d3e133a53d0
jynish102 <jaykal67kal@gmail.com>
2026-05-22 23:39:18 +0530

    react-hook-form,real time editor,select,input,post form,signup,login

--- megablog/src/components/index.js
+++ megablog/src/components/index.js
@@ -3,11 +3,25 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import Logout from "./Header/Logout";
+import Button from "./Button/Button";
+import Select from "./Select/Select";
+import Input from "./Input/Input";
+import RTE from "./RTE/RTE";
+import PostForm from "./post-form/PostForm";
+import Signup from "./Signup";
+import Login from "./Login";
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    Logout
+    Logout,
+    Button,
+    Select,
+    Input,
+    RTE,
+    PostForm,
+    Signup,
+    Login
 }
