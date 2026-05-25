d92bfee878cafb9728ef3fc483d472a717b9d359
jynish102 <jaykal67kal@gmail.com>
2026-05-25 12:55:17 +0530

    new add fillstack.

---
+++ fullstack/backend/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+
+const app = express();
+
+// app.get("/", (req, res) => {
+//   res.json({ message: "Server is running :):)" });
+// });
+
+//route for jokes
+app.get("/jokes", (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      setup: "Why don't scientists trust atoms?",
+      punchline: "Because they make up everything!",
+    },
+    {
+      id: 2,
+      setup: "What do you call a fake noodle?",
+      punchline: "An impasta!",
+    },
+    {
+      id: 3,
+      setup: "Why did the scarecrow win an award?",
+      punchline: "Because he was outstanding in his field!",
+    },
+  ];
+  res.send(jokes );
+});
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Service at https://localhost:${PORT}`);
+});
